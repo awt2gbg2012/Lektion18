c18f54b3d3a49390066061b1b005e50caefbd4ff
David Gullmarsvik <awt2.2012@gmail.com>
2012-09-27 02:17:07 +0200

    Innan övningar

---
+++ Lektion18/Areas/API/APIAreaRegistration.cs
@@ -0,0 +1,58 @@
+﻿using System.Web.Mvc;
+
+namespace Lektion18.Areas.API
+{
+    public class APIAreaRegistration : AreaRegistration
+    {
+        public override string AreaName
+        {
+            get
+            {
+                return "API";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context)
+        {
+            context.MapRoute(
+                "SingleComment",
+                "API/Comments/Comment/{id}",
+                new
+                {
+                    controller = "Comments",
+                    action = "Comment",
+                    id = UrlParameter.Optional
+                }
+            );
+            context.MapRoute(
+                "ListComments",
+                "API/Comments/{page}/{count}",
+                new
+                {
+                    controller = "Comments",
+                    action = "CommentList",
+                    page = UrlParameter.Optional,
+                    count = UrlParameter.Optional
+                }
+            );
+            context.MapRoute(
+                "ListCommentsAll",
+                "API/Comments",
+                new
+                {
+                    controller = "Comments",
+                    action = "CommentList",
+                    page = UrlParameter.Optional,
+                    count = UrlParameter.Optional
+                }
+            );
+
+            context.MapRoute(
+                "Api_default",
+                "API/{controller}/{action}/{id}",
+                new { action = "Index", id = UrlParameter.Optional }
+            );
+
+        }
+    }
+}
---
+++ Lektion18/Areas/API/Models/Abstract/ICommentManager.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Lektion18.Areas.API.Models.Abstract
+{
+    public interface ICommentManager
+    {
+        Comment Create(Comment item);
+        List<int> CreateComments(List<Comment> items);
+        Comment Update(Comment item);
+        Comment GetById(int id);
+        List<Comment> GetComments(int? page, int? count);
+        bool Delete(int id);
+    }
+
+}
